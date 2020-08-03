3c26423454888986e957b7359be6a2c7e70e7820
vadzimv <vysotski.vadim@gmail.com>
2020-08-03 23:12:49 +0300

    added achievements

--- achievements/lib/achievements_list.rb
+++ achievements/lib/achievements_list.rb
@@ -1,2 +1,22 @@
 module Achievements
+
+    class Achievement < Liquid::Drop
+        attr_reader :id, :name, :description, :value
+        def initialize(id:, name:, description:, value:)
+            @id = id
+            @name = name
+            @description = description
+            @reason = reason
+        end
+    end
+
+    module List
+        HELPING_HAND = Achievement.new(id: "helping_hand", name: "Helping Hand", description: "Student helped some other student")
+        HOME_WORK_COMPLETED = Achievement.new(id: "homework_completed", name: "Completed homework", description: "Studend completed homework")
+        HOME_WORK_COMPLETED_2 = Achievement.new(id: "homework_completed", name: "Completed homework", description: "Studend completed homework")
+        HOME_WORK_COMPLETED_3 = Achievement.new(id: "homework_completed", name: "Completed homework", description: "Studend completed homework")
+        HOME_WORK_COMPLETED_4 = Achievement.new(id: "homework_completed", name: "Completed homework", description: "Studend completed homework")
+        HOME_WORK_COMPLETED_5 = Achievement.new(id: "homework_completed", name: "Completed homework", description: "Studend completed homework")
+    end
+
 end
