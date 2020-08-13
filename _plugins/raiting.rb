35839179daf3ab15afb9d044415b87d8320f818c
vadzimv <vysotski.vadim@gmail.com>
2020-08-13 18:39:47 +0300

    use students data

--- _plugins/raiting.rb
+++ _plugins/raiting.rb
@@ -5,11 +5,21 @@ module Rating
   class RatingDataGenerator < Jekyll::Generator
     def generate(site)
       ratingTemplate = site.pages.detect {|page| page.name == 'rating.html'}
-      puts "hi"
+      ratingCalculator = Achievements.new()
+
       students = site.data["students"].map { |student|
           Student.new(telegramId: student["telegramId"], name: student["name"])
       }
-      rating = Achievements.calculateRating(students)
+
+      homeWorks = site.data["homeworks"].map { |homework|
+          HomeWork.new(id: homework["id"], dueDate: DateTime.parse(homework["dueDate"]))
+      }
+      homeWorksReviews = site.data["homework-reviews"].map { |review|
+        HomeWorkReview.new(homeWorkId: review["homeworkId"], mentorId: review["mentorId"], studentId: review["studentId"], homeworkCompletedDate: DateTime.parse(review[homeworkCompleted]))
+      }
+      ratingCalculator.withHomeworks(homeWorks: homeWorks, homeworkReviews: homeWorksReviews)
+
+      rating = ratingCalculator.calculate().studentsRating
       ratingTemplate.data["rating"] = rating
     end
   end
