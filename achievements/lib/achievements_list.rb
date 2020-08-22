803f037b83d013a5b8f8e8daa22e79f4b23cbdb6
ci <ci@github.com>
2020-08-22 18:56:58 +0000

    new version Sat Aug 22 18:56:58 UTC 2020

--- achievements/lib/achievements_list.rb
+++ achievements/lib/achievements_list.rb
@@ -10,13 +10,15 @@ module Achievements
         end
     end
 
-    module List
+    HOME_WORK_GENERIC_DESCRIPTION = "Homework is the most important part of your journey in Android developmenet. Here you can apply all knowledge that you got on the resent lectures, find gaps and even learn something new."
+
+    module List 
         HELPING_HAND = Achievement.new(id: "helping_hand", value: 100, name: "Helping Hand", description: "Student helped some other student")
-        HOME_WORK_COMPLETED_1 = Achievement.new(id: "homework_completed_1", value: 100, name: "Completed homework", description: "Studend completed homework")
-        HOME_WORK_COMPLETED_2 = Achievement.new(id: "homework_completed_2", value: 200, name: "2 Homeworks in the row", description: "Studend completed 2 homeworks in a row in time")
-        HOME_WORK_COMPLETED_3 = Achievement.new(id: "homework_completed_3", value: 300, name: "3 Homeworks in the row", description: "Studend completed 3 homeworks in a row in time")
-        HOME_WORK_COMPLETED_4 = Achievement.new(id: "homework_completed_4", value: 500, name: "4 Homeworks in the row", description: "Studend completed 4 homeworks in a row in time")
-        HOME_WORK_COMPLETED_5 = Achievement.new(id: "homework_completed_5", value: 700, name: "5 Homeworks in the row", description: "Studend completed 5 homeworks in a row in time")
+        HOME_WORK_COMPLETED_1 = Achievement.new(id: "homework_completed_1", value: 100, name: "Completed homework", description: "#{HOME_WORK_GENERIC_DESCRIPTION} Every time you send homework to your mentor before the deadline you get this achievement.")
+        HOME_WORK_COMPLETED_2 = Achievement.new(id: "homework_completed_2", value: 200, name: "2 Homeworks in the row", description: "#{HOME_WORK_GENERIC_DESCRIPTION} To get this achievement complete two homeworks in the row before the dead line.")
+        HOME_WORK_COMPLETED_3 = Achievement.new(id: "homework_completed_3", value: 300, name: "3 Homeworks in the row", description: "#{HOME_WORK_GENERIC_DESCRIPTION} To get this achievement complete three homeworks in the row before the dead line.")
+        HOME_WORK_COMPLETED_4 = Achievement.new(id: "homework_completed_4", value: 500, name: "4 Homeworks in the row", description: "#{HOME_WORK_GENERIC_DESCRIPTION} To get this achievement complete four homeworks in the row before the dead line.")
+        HOME_WORK_COMPLETED_5 = Achievement.new(id: "homework_completed_5", value: 700, name: "5 Homeworks in the row", description: "#{HOME_WORK_GENERIC_DESCRIPTION} To get this achievement complete five homeworks in the row before the dead line.")
 
         ALL_ACHIEVEMENTS = [
             HELPING_HAND,
