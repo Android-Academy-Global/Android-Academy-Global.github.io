cfe1c065819689599f13666adea1cbb34d69f578
vadzim vysotski <v.vysotski@godeltech.com>
2020-09-24 15:37:08 +0300

    Revert "new version Thu Sep 24 12:34:44 UTC 2020"
    This reverts commit 9181e9b1c8b57d307ffb5de9fa59bddaa4aa9d0f.

---
+++ _plugins/raiting.rb
@@ -0,0 +1,77 @@
+require "achievements"
+
+include Achievements
+
+module Rating
+
+  class RatingDataGenerator < Jekyll::Generator
+    def generate(site)
+      ratingTemplate = site.pages.detect {|page| page.name == 'rating.html'}
+      ratingCalculator = AchievementsCalculator.new()
+
+      students = site.data["students"].map { |student|
+        Student.new(telegramId: student["telegramId"], name: student["name"])
+      }
+      ratingCalculator.withStudents(students)
+
+      orderNumber = -1
+      #TODO: order by due date
+      homeWorks = site.data["homeworks"].map { |homework|
+        orderNumber = orderNumber + 1
+        HomeWork.new(id: homework["id"], dueDate: parseDate(homework["dueDate"]), orderNumber: orderNumber)
+      }
+      homeWorksReviews = site.data["homework-reviews"].map { |review|
+        HomeWorkReview.new(homeWorkId: review["homeworkId"], mentorId: review["mentorId"], studentId: review["studentId"], homeworkCompletedDate: parseDate(review["homeworkCompleted"]))
+      }
+      ratingCalculator.withHomeworks(homeWorks: homeWorks, homeworkReviews: homeWorksReviews)
+
+      studentsHelps = site.data["students-helps"].map { |help|
+        StudentHelp.new(studentIdHowHelped: help["from"], studentIdWhoGotHelp: help["to"], comment: help["comment"])
+      }
+      ratingCalculator.withStudentsHelp(studentsHelps)
+
+      rating = ratingCalculator.calculate().studentsRating
+      ratingTemplate.data["rating"] = rating
+
+      rating.each do |studentInRating|
+        site.pages << StudentPage.new(site, studentInRating)
+      end
+
+      List::ALL_ACHIEVEMENTS.each do |achievement|
+        site.pages << AchievementPage.new(site, achievement)
+      end
+    end
+
+    def parseDate(date)
+      return Date.strptime("12/22/2011", "%m/%d/%Y")
+    end
+  end
+
+
+  class StudentPage < Jekyll::Page
+    def initialize(site, studentInRating)
+      @site = site
+      @base = site.source
+      @dir = File.join('students', studentInRating.student.telegramId)
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(@base, '_layouts'), "student.html")
+      self.data["studentInRating"] = studentInRating
+    end
+  end
+
+  class AchievementPage < Jekyll::Page
+    def initialize(site, achievement)
+      @site = site
+      @base = site.source
+      @dir = File.join('achievements', achievement.id)
+      @name = 'index.html'
+
+      self.process(@name)
+      self.read_yaml(File.join(@base, '_layouts'), "achievement.html")
+      self.data["achievement"] = achievement
+    end
+  end
+
+end
