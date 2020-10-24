524824bf4fd3af126492cc4e915c5376ff5fe640
vadzimv <vysotski.vadim@gmail.com>
2020-10-24 17:35:17 +0300

    fetch all students

--- sync-docs.sh
+++ sync-docs.sh
@@ -1,7 +1,7 @@
-AIRTABLE_TOEN=$1
+AIRTABLE_TOKEN=$1
 
-curl "https://api.airtable.com/v0/appFDFb3CkxWHXrd5/Users?view=All%20participants" -H "Authorization: Bearer ${AIRTABLE_TOEN}" > ./_data/users.json
-bundle exec ruby convert-students.ruby > ./_data/students.csv
+#curl "https://api.airtable.com/v0/appFDFb3CkxWHXrd5/Users?view=All%20participants" -H "Authorization: Bearer ${AIRTABLE_TOEN}" > ./_data/users.json
+bundle exec ruby fetch-students.ruby $AIRTABLE_TOKEN > ./_data/students.csv
 ## curl https://docs.google.com/spreadsheets/d/1cBI0lZsgGoZkdREyhBSgkI6fFSLHLIlSTNW7JCeO3hU/gviz/tq?tqx=out:csv > ./_data/students.csv
 
 curl https://docs.google.com/spreadsheets/d/1Sz6IRJWDhzJLJnlAtrQknTsw470nTe5enPr8k3bXs1Q/gviz/tq?tqx=out:csv > ./_data/homeworks.csv
