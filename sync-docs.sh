08c97cb3325935c7364b05ff6d371cdd9ebc4fef
ci <ci@github.com>
2020-11-02 11:43:31 +0000

    new version Mon Nov  2 11:43:31 UTC 2020

--- sync-docs.sh
+++ sync-docs.sh
@@ -8,8 +8,7 @@ wc -l ./_data/students.csv
 
 curl https://docs.google.com/spreadsheets/d/1Sz6IRJWDhzJLJnlAtrQknTsw470nTe5enPr8k3bXs1Q/gviz/tq?tqx=out:csv > ./_data/homeworks.csv
 curl "https://docs.google.com/spreadsheets/d/1ycYPofJUu3bQ4hyV6RoNzZydKVEpvqPQtFHHBPs8pvI/gviz/tq?tqx=out:csv&sheet=homeworks" > ./_data/homework-reviews.csv
-
-curl https://docs.google.com/spreadsheets/d/1Ac8EkZhl2R4dDjMtTsC1RtEJjMtqEFf2bk1-SVwnwi8/gviz/tq?tqx=out:csv > ./_data/best-questions.csv
+curl "https://docs.google.com/spreadsheets/d/1ycYPofJUu3bQ4hyV6RoNzZydKVEpvqPQtFHHBPs8pvI/gviz/tq?tqx=out:csv&sheet=best_questions" > ./_data/best-questions.csv
 
 curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWOV7JurhlvU/gviz/tq?tqx=out:csv&sheet=kotlin" > ./_data/kotlin-attendees.csv
 curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWOV7JurhlvU/gviz/tq?tqx=out:csv&sheet=project" > ./_data/project-attendees.csv
