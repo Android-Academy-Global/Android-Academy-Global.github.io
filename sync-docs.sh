c53e8fb69e1dd6df646ed1eee881464359180349
vadzimv <vysotski.vadim@gmail.com>
2020-11-30 09:41:01 +0300

    connected list feedbacks

--- sync-docs.sh
+++ sync-docs.sh
@@ -1,4 +1,4 @@
-AIRTABLE_TOKEN=$1
+AIRTABLE_TOKEN=${1:-$ANDROID_ACADEMY_AIRTABLE_TOKEN}
 
 #curl "https://api.airtable.com/v0/appFDFb3CkxWHXrd5/Users?view=All%20participants" -H "Authorization: Bearer ${AIRTABLE_TOEN}" > ./_data/users.json
 bundle exec ruby fetch-students.ruby $AIRTABLE_TOKEN > ./_data/students.csv
@@ -14,6 +14,7 @@ curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWO
 curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWOV7JurhlvU/gviz/tq?tqx=out:csv&sheet=project" > ./_data/project-attendees.csv
 curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWOV7JurhlvU/gviz/tq?tqx=out:csv&sheet=view" > ./_data/view-attendees.csv
 curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWOV7JurhlvU/gviz/tq?tqx=out:csv&sheet=components" > ./_data/components-attendees.csv
+curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWOV7JurhlvU/gviz/tq?tqx=out:csv&sheet=lists" > ./_data/lists-attendees.csv
 #TODO: add each workshop
 
 curl https://docs.google.com/spreadsheets/d/1U2TRkH_8oiHnzacwkjzXD-q2XhqvmAIp759iMYxlXKI/gviz/tq?tqx=out:csv > ./_data/students-helps.csv
