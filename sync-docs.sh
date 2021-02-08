0cff9989b17f9e3c32907001d39ae16c47776cf4
ci <ci@github.com>
2021-02-08 18:26:18 +0000

    new version Mon Feb  8 18:26:18 UTC 2021

--- sync-docs.sh
+++ sync-docs.sh
@@ -20,6 +20,7 @@ curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWO
 curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWOV7JurhlvU/gviz/tq?tqx=out:csv&sheet=networking" > ./_data/networking-attendees.csv
 curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWOV7JurhlvU/gviz/tq?tqx=out:csv&sheet=persistency" > ./_data/persistency-attendees.csv
 curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWOV7JurhlvU/gviz/tq?tqx=out:csv&sheet=background" > ./_data/background-attendees.csv
+curl "https://docs.google.com/spreadsheets/d/1hDvboEzKWpmYxl9VNzXKnY9ph8FBNKyDWOV7JurhlvU/gviz/tq?tqx=out:csv&sheet=notifications" > ./_data/notifications-attendees.csv
 #TODO: add each workshop
 
 curl https://docs.google.com/spreadsheets/d/1U2TRkH_8oiHnzacwkjzXD-q2XhqvmAIp759iMYxlXKI/gviz/tq?tqx=out:csv > ./_data/students-helps.csv
